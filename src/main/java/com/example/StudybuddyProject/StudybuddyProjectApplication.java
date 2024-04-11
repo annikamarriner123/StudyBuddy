99ee78d9f4dab1e062276e8b22ae38d1179e7b23
Glen Garcia <gargl791@student.otago.ac.nz>
2024-04-11 16:00:47 +1200

    add springboot initializer files for project

---
+++ src/main/java/com/example/StudybuddyProject/StudybuddyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudybuddyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybuddyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudybuddyProjectApplication.class, args);
+	}
+
+}
