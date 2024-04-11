c9d2ac0ddd06ca81656beb06366cb44a54d17c85
Glen Garcia <gargl791@student.otago.ac.nz>
2024-04-11 16:28:59 +1200

    bruh

--- src/main/java/otago/StudybuddyProject/StudybuddyProjectApplication.java
+++ src/main/java/otago/StudybuddyProject/StudybuddyProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.StudybuddyProject;
+package otago.StudybuddyProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
