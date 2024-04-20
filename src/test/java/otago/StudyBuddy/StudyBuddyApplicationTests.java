d7120a77417b8f86588cad56862bd3116f350336
Glen Garcia <gargl791@student.otago.ac.nz>
2024-04-20 16:54:03 +1200

    test ci integration

--- src/test/java/otago/StudyBuddy/StudyBuddyApplicationTests.java
+++ src/test/java/otago/StudyBuddy/StudyBuddyApplicationTests.java
@@ -1,14 +1,22 @@
 package otago.StudyBuddy;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import otago.StudyBuddy.service.UserService;
 
 @SpringBootTest
 class StudyBuddyApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
+    
 	@Test
 	void contextLoads() {
-		
+		assertThat(userService).isNotNull();
 	}
+        
 
 }
