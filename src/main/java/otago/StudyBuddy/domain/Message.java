9e718f2900544a349a27f0be84273c732fcb92dc
William <donwi381@student.otago.ac.nz>
2024-04-22 14:02:50 +1200

    Create Message.java

---
+++ src/main/java/otago/StudyBuddy/domain/Message.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package otago.StudyBuddy.domain;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author willi
+ */
+public class Message {
+    private Integer messageId;
+    private Integer senderId;
+    private Integer recipientId;
+    private String content;
+    private Timestamp timestamp;
+
+    
+    public Message (){}
+    
+    public Message(Integer messageId, Integer senderId, Integer recipientId, String content, Timestamp timestamp){
+        this.messageId = messageId;
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public Integer getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Integer messageId) {
+        this.messageId = messageId;
+    }
+
+    public Integer getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Integer senderId) {
+        this.senderId = senderId;
+    }
+
+    public Integer getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(Integer recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" + "messageId=" + messageId + ", senderId=" + senderId + ", recipientId=" + recipientId + ", content=" + content + ", timestamp=" + timestamp + '}';
+    }
+    
+    
+}
