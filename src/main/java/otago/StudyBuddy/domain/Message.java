83e356872d9286c1ef724b0723f3f15cf1bf7f16
gargl791 <131325490+gargl791@users.noreply.github.com>
2024-04-30 22:37:53 +1200

    Update Message.java

--- src/main/java/otago/StudyBuddy/domain/Message.java
+++ src/main/java/otago/StudyBuddy/domain/Message.java
@@ -13,6 +13,7 @@ import java.sql.Timestamp;
 @Table(name = "messages")
 public class Message {
 
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer messageId;
