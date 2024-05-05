ccc6b97036377c3787e5b3586f3e7c4206f82a65
maran320 <maran320@student.otago.ac.nz>
2024-05-05 15:36:20 +1200

    working on nabigation/header

---
+++ src/main/resources/static/js/accountsHeader.js
@@ -0,0 +1,19 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+
+document.write(`
+<header>
+    <div class="header-content">
+        <nav>
+            <ul>
+                <li><a href="log-in">LOG IN</a></li>
+                <li><a href="sign-up">SIGN UP</a></li>
+            </ul>
+        </nav>
+        <h2><a href="home">Study Buddy</a></h2>
+    </div>
+</header>
+`);
