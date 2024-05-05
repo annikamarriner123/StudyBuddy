9579a2c544ea49f9b9f23155e4ebb21b179e29fb
maran320 <maran320@student.otago.ac.nz>
2024-05-05 14:22:08 +1200

    making header one location

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,27 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+
+document.write(`
+<header>
+    <div class="container">
+        <div class="banner">
+            <h2><a href="/home">Study Buddy</a></h2>
+            <a href="/findStudyPeers">Find Study Peers</a>
+            <a href="/team">Teams</a>
+            <a href="/chat">Inbox</a>
+            <a href="/updatePapers">Add Papers</a>
+            <div class="dropdown">
+                <button class="dropbtn">Menu</button>
+                <div class="dropdown-content">
+                    <a href="/update-details">Account Info</a>
+                    <a href="/logout" id="log-out">Log Out</a>
+                    <a href="/home">Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+`);
