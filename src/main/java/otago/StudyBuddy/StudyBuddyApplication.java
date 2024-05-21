14834f53688dc1e1dc45bac7dfed82baa2f0f7cf
glen <gargl791@student.otago.ac.nz>
2024-05-21 14:37:37 +1200

    change port of springboot project to 8081

--- src/main/java/otago/StudyBuddy/StudyBuddyApplication.java
+++ src/main/java/otago/StudyBuddy/StudyBuddyApplication.java
@@ -8,7 +8,7 @@ public class StudyBuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyBuddyApplication.class, args);
-                System.out.println("Running on: http://localhost:8080/\n\n");
+                System.out.println("Running on: http://localhost:8081/\n\n");
                 
 	}
 
